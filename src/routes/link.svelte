508172064dd1864d530ce7a0f468ca13b3c2c102
dethMastery <owner@detzz.in.th>
2022-04-23 03:11:35 +0700

    added: link page

---
+++ src/routes/link.svelte
@@ -0,0 +1,25 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<svelte:head>
+	<title>Link Video/Audio Player</title>
+</svelte:head>
+
+<section>
+
+</section>
+	Just Entering Your Link:
+	<form action="/link">
+		
+	</form>
+<style>
+	section {
+		width: 100%;
+		height: 100vh;
+		
+		display: flex;
+		justify-content: center;
+		vertical-align: middle;
+	}
+</style>
